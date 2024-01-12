a01dd377ded0efad8bfde1a8f48818ccc0526c7b
alfuadi <57789372+alfuadi@users.noreply.github.com>
2024-01-12 09:06:11 +0700

    Add time buttons

--- Test.py
+++ Test.py
@@ -4,47 +4,45 @@ from io import BytesIO
 import requests
 import datetime
 
-def get_images(dtype):
-    image_urls = [
-        f"https://publik.bmkg.go.id/cawo/inacawo/InaCAWO_{dtype}_6hr_Indo_{(datetime.datetime.today().replace(hour=0)+datetime.timedelta(hours=dh)).strftime('%Y%m%d%H')}0000.png" for dh in range(12,72,6)
-    ]
-    print(image_urls)
-    images = [Image.open(BytesIO(requests.get(url).content)) for url in image_urls]
-    return images
+def on_button_click(dtype,selected_time):
+    url = f"https://publik.bmkg.go.id/cawo/inacawo/InaCAWO_{dtype}_6hr_Indo_{selected_time}0000.png"  # Ganti dengan path sesuai struktur direktori Anda
+    image_path = Image.open(BytesIO(requests.get(url).content))
+    st.image(image_path, caption=f"Image {selected_time}", use_column_width=True)
 
-def imview(images,num_images):
-    session_state = st.session_state
-    if "current_index" not in session_state:
-        session_state.current_index = 0
+def get_nearest_multiple_of_six_hours():
+    current_time = datetime.datetime.now()
+    rounded_hour = (current_time.hour // 6) * 6  # Mendapatkan kelipatan 6 jam terdekat
+    return current_time.replace(hour=rounded_hour, minute=0, second=0, microsecond=0)
 
-    if num_images == 0:
-        st.error("Tidak ada file PNG dalam direktori yang diberikan.")
-        return
+list_time = [(get_nearest_multiple_of_six_hours() + datetime.timedelta(hours=dh)).strftime('%Y%m%d%H') for dh in range(6, 72, 6)]
 
-    # Menampilkan gambar saat ini
-    st.image(images[session_state.current_index])
-
-    # Tombol Previous
-    if st.button("Previous") and session_state.current_index > 0:
-        session_state.current_index -= 1
-
-    # Tombol Next
-    if st.button("Next") and session_state.current_index < num_images - 1:
-        session_state.current_index += 1
+def create_buttons():
+    button_clicked = None
+    columns = st.columns(len(list_time))
+    time1=None
+    for i, time in enumerate(list_time):
+        with columns[i]:
+            button_label = f"{time[6:8]}\n{time[8:10]}"
+            if st.button(button_label):
+                button_clicked = button_label
+                time1 = list_time[i]
+            
+    return button_clicked,time1
+def imview(button_clicked,dtype,time):
+    if button_clicked is not None and time is not None:
+        on_button_click(dtype,time)
+    else:
+        st.info("Klik salah satu waktu")
 
 def main():
     st.title("InaCAWO Data Display Platform (BETA)")
-
+    
     # Menampilkan tombol untuk memilih gambar berdasarkan timestamp
     datatypes = ["Mean", "POE5", "POE10", "POE20", "POE50"]
-    selected_type = st.selectbox("Select a data type", datatypes)
-
-    # Menampilkan gambar berdasarkan timestamp yang dipilih
-    dtype = selected_type
-    images = get_images(dtype)
-    num_images = len(images)
-
-    imview(images,num_images)
+    dtype = st.selectbox("Select a data type", datatypes)
+    button_clicked,time=create_buttons()
+    print(time)
+    imview(button_clicked,dtype,time)
 
 if __name__ == "__main__":
     main()
