31fcb7e3177cf915609f7c856cdf6f59cce770ac
alfuadi <57789372+alfuadi@users.noreply.github.com>
2024-01-19 11:11:00 +0700

    Update Verification_app.py

--- Verification_app.py
+++ Verification_app.py
@@ -86,7 +86,7 @@ else:
         datelist = [datetime.datetime(2024,1,1)+datetime.timedelta(dt) for dt in range(31)]
         bof = pd.DataFrame({'fct':fct_name, 'Time':datelist})
         dbof = pd.merge(bof,filtered_df, on='Time')
-        acc_fct = dbof[['Time','ftc','Accuracy_WW','Accuracy_WW']].groupby('fct').mean().reset_index()
+        acc_fct = dbof[['Time','fct','Accuracy_WW','Accuracy_WW']].groupby('fct').mean().reset_index()
         st.dataframe(acc_fct)
 
     # Hitung nilai statistik
