4458df40e2769e74fd497e1fcf79ba43655bcb30
alfuadi <57789372+alfuadi@users.noreply.github.com>
2024-01-19 10:38:27 +0700

    Update Verification_app.py

--- Verification_app.py
+++ Verification_app.py
@@ -113,7 +113,6 @@ else:
 # ===============================Tampilkan tabel statistik================================
 # Grafik Time Series untuk Rata-rata Agregasi Seluruh Provinsi
 st.header('Time Series Graph')
-filtered_df['Time_str']=filtered_df['Time'].dt.strftime('%d-%m-%Y')
-st.write(filtered_df.dropna().groupby('Time_str'))
+st.write(filtered_df.dropna().groupby('Time'))
 avg_df = filtered_df.groupby('Time_str').mean().reset_index()
 fig_time_series = st.line_chart(avg_df, x='Time', y=['Accuracy_WW', 'Accuracy_GSMaP'])
